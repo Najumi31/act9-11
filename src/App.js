fbaf51a0a97bc916ad09a9010c958b0ef4c45754
Najumi31 <jadeestremera@thelewiscollege.edu.ph>
2023-12-08 03:59:24 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
 
-function App() {
+// Style Object Variables
+
+const formStyle = {
+  maxWidth: '300px',
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '10px',
+  border: '1px solid #ccc',
+  borderRadius: '5px',
+  textAlign: 'center',
+  fontFamily: 'Sans-serif',
+  background: 'linear-gradient(to bottom, white, gray)',
+};
+
+const inputStyle = {
+  width: '100%',
+  padding: '10px',
+  margin: '8px 0',
+  display: 'inline-block',
+  border: '1px solid #ccc',
+  borderRadius: '4px',
+  boxSizing: 'border-box',
+};
+
+const buttonStyle = {
+  backgroundColor: '#4CAF50',
+  color: 'white',
+  padding: '10px 15px',
+  margin: '8px 0',
+  border: 'none',
+  borderRadius: '4px',
+  cursor: 'pointer',
+};
+
+const buttonStyle2 = {
+  backgroundColor: 'blue',
+  color: 'white',
+  padding: '10px 15px',
+  margin: '8px 0',
+  border: 'none',
+  borderRadius: '4px',
+  cursor: 'pointer',
+};
+
+const buttonStyle3 = {
+  backgroundColor: 'gray',
+  color: 'white',
+  padding: '10px 15px',
+  margin: '8px 0',
+  border: 'none',
+  borderRadius: '4px',
+  cursor: 'pointer',
+};
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Display an alert with the entered username and password
+    alert(`Logging in with:\nUsername: ${username}\nPassword: ${password}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={formStyle}>
+      <h1>LOGIN</h1>
+      <label>
+        Username
+        <input type="text" style={inputStyle} value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password
+        <input type="password" style={inputStyle} value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button style={buttonStyle} onClick={handleLogin}>
+        Login
+      </button>
+      <p>------------ or ------------</p>
+      <button style={buttonStyle2} onClick={() => alert('Login to Facebook')}>
+        Login to Facebook
+      </button>
+      <button style={buttonStyle3} onClick={() => alert('Login to Google')}>
+        Login to Google
+      </button>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
 
 export default App;
+
+
+
